f34da1fdc10760fe1384eeb41b4c41c0158f4d4e
Mykhailo-Gogol <68640759+Mykhailo-Gogol@users.noreply.github.com>
2021-02-15 13:05:05 +0200

    Додає секцію who

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-function burgerMenu(selector){
-    let menu = $(selector);
-    let button = menu.find('.burger-menu_button');
-    let links = menu.find('.burger-menu_link');
-    let overlay = menu.find('.burger-menu_overlay');
+function burgerMenu(selector) {
+  let menu = $(selector);
+  let button = menu.find(".burger-menu_button");
+  let links = menu.find(".burger-menu_link");
+  let overlay = menu.find(".burger-menu_overlay");
 
-    button.on('click', (e) => {
-        e.preventDefault();
-        toggleMenu();
-    });
-    links.on('click', () => toggleMenu());
-    overlay.on('click', () => toggleMenu());
+  button.on("click", (e) => {
+    e.preventDefault();
+    toggleMenu();
+  });
+  links.on("click", () => toggleMenu());
+  overlay.on("click", () => toggleMenu());
 
-function toggleMenu() {
-    menu.toggleClass('burger-menu_active');
-    }
+  function toggleMenu() {
+    menu.toggleClass("burger-menu_active");
+  }
 }
-burgerMenu('.burger-menu');
+burgerMenu(".burger-menu");
